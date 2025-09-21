734203cefd54e53721c59c371db11ee8b44b5bb4
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2025-09-21 13:54:24 +0530

    Restart

--- src/config/env.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  appwrite: {
-    projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECTID,
-    endpoint: process.env.NEXT_PUBLIC_APPWRITE_API_ENDPOINT,
-    apiKey: process.env.APPWRITE_API_KEY,
-  },
-};
